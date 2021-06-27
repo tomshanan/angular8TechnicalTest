f7a84ae05a2ad2f0223c17283850f27f73380462
Tom Shanan <tomshanan@gmail.com>
2021-06-27 20:54:15 +0100

    added functionality to add and remove todos in the main component

--- tech-test/src/app/app.component.ts
+++ tech-test/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tech-test';
+  todos$ = this.todoService.todos$.asObservable()
+  newTodo:string;
+
+  constructor(
+    private todoService: TodoService
+    ){
+      this.todoService.getAll()
+  }
+
+  createTodo(todoText: string){
+    if(todoText === '' && todoText == null) return;
+    this.todoService.createTodo({
+      id : null,
+      label: null,
+      description: todoText,
+      category: null,
+      done: null
+    })
+    this.newTodo = ''
+  }
+
+  deleteTodo(todo:Todo){
+    this.todoService.deleteTodo(todo.id)
+  }
+
+  updateTodo(todo:Todo){
+    this.todoService.updateTodo(todo)
+  }
+
+  trackByFn(index,item){
+    return item.id
+  }
 }
